eff4a91e6742cb8d9c7adbc0673721ebcea1ec85
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-04-29 12:06:50 +0000

    new changes variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =12345
+let emailId ="hitesh@apple.com"
+var accountPassword ="12345"
+accountCity="Pune"
+
+
+// accountId =2 // it cannot be changed , it is not allowed
+
+
+emailId ="hc@google.com"
+accountPassword="345"
+accountCity="Dublin"
+
+console.table([accountId,emailId,accountPassword,accountCity])
