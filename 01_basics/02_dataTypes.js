65dbe38879cb8f515c3646797f53ada6438a0f65
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-05-05 10:39:37 +0000

    Data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use Strict";  // treat all js code as newer version
+
+// alert(3+3) we are using node js and not browser
+
+let name ="Hitesh"
+let age =13
+let isLoggedIn = false
+
+//data Types ( primitive data types)
+// numbers => 2 to power 53
+// bigInt
+// String ==> " "
+// boolean => True or false
+// null  => standalone value ex -> let state = null
+// undefined ==> value not defined ex -> let state;
+// symbol => unique 
+
+// Object 
+
+console.log(typeof null); // -> Object
+console.log(typeof undefined);  // -> undefined 
