73f833f29ce0a7ba172ba63e3b72674683a6e6ef
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-05-05 10:54:54 +0000

    Conversion of data types i.e Number, String, Boolean

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+
+let score = "33"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+
+// "33" => 33
+// "33abc" => NaN - > Not a Number
+//  true => 1 or false => 0
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => True and 0 => false
+// "hitesh" => True
+// "" => false
+
+
+let someNumber = 33
+let stringNum = String(someNumber);
+console.log(stringNum);
+console.log(typeof stringNum);
+
